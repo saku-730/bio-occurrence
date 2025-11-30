55efce6e8995b6846f97f26974d5d433bc1101a4
saku <saku112358@gmail.com>
2025-12-01 07:47:54 +0900

    front

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/saku-730/bio-occurrence/backend
+
+go 1.25.0
---
+++ frontend/next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+	output: 'export',
+	image:{
+		unoptimized: true,
+	},
+  /* config options here */
+};
+
+export default nextConfig;
